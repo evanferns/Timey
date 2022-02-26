49dc593cccd37dcf4a9819f474582d79123fa5c1
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2022-02-26 17:25:14 -0500

    Created the sessionLinkPage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import StartPage from "./components/start-page/startPage.jsx";
+import SessionInputPage from "./components/session-input-page/sessionInputPage.jsx";
+import SessionLinkPage from "./components/session-link/sessionLinkPage.jsx";
 
 function App() {
   return (
